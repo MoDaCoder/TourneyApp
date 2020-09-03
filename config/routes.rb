399a4835f32650cc4f25298629fdd0fd5b63b2b0
mohehexd <dumblemoe@gmail.com>
2020-09-03 15:40:46 -0500

    Creating a welcome page with options for the user to pick from including matches.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :matches
   devise_for :users, :controllers => { omniauth_callbacks: "callbacks" }
   root to: 'application#home'
+  resources :matches
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
  
