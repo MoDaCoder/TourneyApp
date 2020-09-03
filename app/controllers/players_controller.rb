98636133e7796a3809b7754cb8ea214b3f3f0907
mohehexd <dumblemoe@gmail.com>
2020-09-03 15:57:15 -0500

    Creating a player controller.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,49 @@
 class PlayersController < ApplicationController
+    before_action :find_match, only:[:show, :edit, :update, :destroy]
+
+    #show /show all page
+    def index
+        @player = Player.all
+    end
     
+    #Get /create action
+    def create 
+        @player = Player.new(name: params[:player][:name], player_number: params[:player][:player_number])
+        if @player.valid?
+            @player.save
+            redirect_to player_path(@player.id)
+        else
+            @errors = @player.errors.full_messages
+            render :new    
+        end    
+    end
+    
+    #Get /render new form
+    def new
+        @player = Player.new
+    end
+    
+    #Get /edit form 
+    def edit
+    end
+    
+    #Get /read action for a single object
+    def show 
+    end 
+    
+    #Get /update action
+    def update
+    end 
+    
+    #Get /destroy action
+    def destroy 
+        @player.destroy
+        redirect_to players_path
+    end
+
+    private
+    def find_match
+        @player = Player.find(params[:id])
+    end
 end
+
