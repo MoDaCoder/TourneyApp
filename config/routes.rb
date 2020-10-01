d2eff1359782f36ac400133c5e907fdb8f26399b
Guillermo Martinez <dumblemoe@gmail.com>
2020-10-01 18:10:15 -0500

    adding nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   devise_for :users, :controllers => { omniauth_callbacks: "callbacks" }
   root to: 'application#home'
-  resources :matches
-  resources :players
+  #resources :matches
+  #resources :players
+  resources :matches do 
+    resources :players, shallow: true
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
  
